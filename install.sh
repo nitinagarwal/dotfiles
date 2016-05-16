acd06fe09accfa751c11831002a58968b24d3442
nitin_agarwal <agarwal@uci.edu>
2016-05-15 20:25:20 -0700

    updated files

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc tmux tmux.conf vim zshrc oh-my-zsh"    # list of files/folders to symlink in homedir
+files="bashrc tmux tmux.conf vim zshrc"    # list of files/folders to symlink in homedir
 
 ##########
 
